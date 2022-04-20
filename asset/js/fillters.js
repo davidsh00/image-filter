0b17dc8ac7b181e8f1e99cc29b1c4ea08b7c22e0
davidsh <davoodsh000@gmail.com>
2022-04-20 13:57:14 +0430

    completed

--- asset/js/fillters.js
+++ asset/js/fillters.js
@@ -1,80 +1,160 @@
- export const filters=[
-    {
-      name: "filter1",
-      styles: {
-        filter: "grayscale(0.5)",
+export const filters = [
+  {
+    name: "filter1",
+    styles: [
+      {
+        type: "range",
+        value: 100,
+        style: {
+          filter: "grayscale(0.5)",
+        },
       },
-    },
-    {
-      name: "filter2",
-      styles: {
-        filter: "grayscale(50)",
+    ],
+  },
+  {
+    name: "filter2",
+    styles: [
+      {
+        type: "range",
+        value: 100,
+        style: {
+          filter: "grayscale(1)",
+        },
       },
-    },
-    {
-      name: "filter3",
-      styles: {
-        filter: "blur(1px)",
+    ],
+  },
+  {
+    name: "filter2",
+    styles: [
+      {
+        type: "range",
+        value: 100,
+        style: {
+          filter: "grayscale(1)",
+        },
       },
-    },
-    {
-      name: "filter4",
-      styles: {
-        filter: "contrast(200%)",
+    ],
+  },
+  {
+    name: "filter2",
+    styles: [
+      {
+        type: "range",
+        value: 100,
+        style: {
+          filter: "brightness(200%)",
+        },
       },
-    },
-    {
-      name: "filter5",
-      styles: {
-        filter: "hue-rotate(90deg)",
+      {
+        type: "range",
+        value: 100,
+        style: {
+          transform:"scale(1.5)",
+        },
       },
-    },
-    {
-      name: "filter6",
-      styles: {
-        filter: "brightness(200%)",
+    ],
+  },
+  {
+    name: "filter3",
+    styles: [
+      {
+        type: "range",
+        value: 1,
+        style: {
+          filter: "blur(1px)",
+        },
       },
-    },
-    {
-      name: "filter7",
-      styles: {
-        filter: "saturate(800%)",
+    ],
+  },
+  {
+    name: "filter4",
+    styles: [
+      {
+        type: "range",
+        value: 100,
+        style: {
+          filter: "contrast(200%)",
+        },
       },
-    },
-    {
-      name: "filter8",
-      styles: {
-        filter: "sepia(100%)",
+    ],
+  },
+  {
+    name: "filter5",
+    styles: [
+      {
+        type: "range",
+        value: 25,
+        style: {
+          filter: "hue-rotate(90deg)",
+        },
       },
-    },
-    {
-      name: "filter9",
-      styles: {
-        filter: "brightness(150%) contrast(200%)",
+    ],
+  },
+  {
+    name: "filter6",
+    styles: [
+      {
+        type: "range",
+        value: 100,
+        style: {
+          filter: "brightness(200%)",
+        },
       },
-    },
-    {
-      name: "filter10",
-      styles: {
-        filter: "invert(100%)",
+    ],
+  },
+  {
+    name: "filter7",
+    styles: [
+      {
+        type: "range",
+        value: 100,
+        style: {
+          filter: "saturate(800%)",
+        },
       },
-    },
-    {
-      name: "filter8",
-      styles: {
-        filter: "sepia(100%)",
+    ],
+  },
+  {
+    name: "filter8",
+    styles: [
+      {
+        type: "range",
+        value: 100,
+        style: {
+          filter: "sepia(100%)",
+        },
       },
-    },
-    {
-      name: "filter9",
-      styles: {
-        filter: "brightness(150%) contrast(200%)",
+    ],
+  },
+  {
+    name: "filter9",
+    styles: [
+      {
+        type: "range",
+        value: 100,
+        style: {
+          filter: "invert(100%)",
+        },
       },
-    },
-    {
-      name: "filter10",
-      styles: {
-        filter: "invert(100%)",
+    ],
+  },
+  {
+    name: "filter10",
+    styles: [
+      {
+        type: "range",
+        value: 20,
+        style: {
+          filter: "hue-rotate(50deg)",
+        },
       },
-    },
-  ];
+      {
+        type: "range",
+        value: 9,
+        style: {
+          transform: "rotate(30deg)",
+        },
+      },
+    ],
+  },
+];
--- asset/js/main.js
+++ asset/js/main.js
@@ -3,23 +3,36 @@ const main = document.getElementsByTagName("main")[0];
 const imageContainer = document.querySelector(".image-container");
 const filtersList = document.querySelector(".filters-list");
 let imgSrc = "";
+
+//filter-controller-container
+const controllerContainer = document.querySelector(
+  ".filter_controller-container"
+);
+const controllerClose = document.querySelector(
+  ".filter_controller-container .close-controller"
+);
+const controllerList = document.querySelector(
+  ".filter_controller-container .list-controller"
+);
+
 import { filters } from "./fillters.js";
-
-
-
+function catchStyles(elem, stylesArr) {
+  stylesArr.forEach((styleobj) => {
+    setstyle(elem, styleobj);
+  });
+}
 function setstyle(elem, styleObj) {
-  for (const property in styleObj) {
-    elem.style[property] = styleObj[property];
+  for (const property in styleObj["style"]) {
+    elem.style[property] = styleObj["style"][property];
   }
 }
 
-function createImageNode(styles = {}, customClass = "") {
+function createImageNode(styles = [], customClass = "") {
   const img = document.createElement("img");
   img.alt = "img";
   img.className = `img ${customClass}`;
   img.src = imgSrc;
-  setstyle(img, styles);
-
+  catchStyles(img, styles);
   return img;
 }
 
@@ -28,17 +41,36 @@ function appendFilterItems(filters) {
     filtersList.append(createFilterItems(filterItem.name, filterItem.styles));
   });
 }
+function appendFilterController(where, stylesArr) {
+  let controllers = [];
+  //fillter range styles
+  const rangeStyles = stylesArr.filter((style) => style.type == "range");
+  rangeStyles.forEach((rangestyle) => {
+    controllers.push(
+      createController(
+        Object.keys(rangestyle.style)[0],
+        rangestyle.value,
+        rangestyle
+      )
+    );
+  });
 
-function changeStyle(itemstyle){
-  const elem=document.querySelector('.big-image')
-setstyle(elem,itemstyle)
+  where.innerHTML = "";
+  where.append(...controllers);
+}
+function changeStyle(itemstyles) {
+  const bigImage = document.querySelector(".big-image");
+  controllerContainer.classList.remove("hide");
+  controllerContainer.classList.add("show");
+  appendFilterController(controllerList, itemstyles);
+  catchStyles(bigImage, itemstyles);
 }
 
 function createFilterItems(id, styles) {
   const filterItem = document.createElement("div");
   filterItem.className = "filter-item";
   filterItem.id = id;
-  filterItem.addEventListener('click',()=>changeStyle(styles))
+  filterItem.addEventListener("click", () => changeStyle(styles));
   filterItem.append(createImageNode(styles));
 
   const text = document.createElement("p");
@@ -61,13 +93,85 @@ function dropHandler(ev) {
 function after_upload(src) {
   imgSrc = src;
   main.classList = "filter";
-  const bigImage = createImageNode({}, "big-image");
+  const bigImage = createImageNode([], "big-image");
   imageContainer.append(bigImage);
   appendFilterItems(filters);
 }
 
 function handleupload(e) {
   after_upload(URL.createObjectURL(this.files[0]));
+}
+
+function captalize(str) {
+  return str[0].toUpperCase() + str.slice(1);
+}
+console.log("hue-rotate".includes("rotate"));
+function prefix(property, propertyVal, subProperty) {
+  if (subProperty.includes("rotate")) {
+    return `${propertyVal * 3.6}deg`;
+  }
+  switch (property) {
+    case "filter":
+      switch (subProperty) {
+        case "blur":
+          return `${propertyVal}px`;
+        default:
+          return `${propertyVal}%`;
+      }
+    case "transform":
+      switch (subProperty) {
+        case "rotate":
+          return `${propertyVal * 3.6}deg`;
+        case "scale":
+          if (propertyVal < 30) {
+            return `${(propertyVal * 10) / 3 / 100}`;
+          } else {
+            return `${propertyVal / 100 + 0.7}`;
+          }
+      }
+    default:
+      return propertyVal;
+  }
+}
+
+function changeByRange(e, id, style) {
+  const bigImage = document.querySelector(".big-image");
+  if (id == "filter" || id == "transform") {
+    const subProperty = style.style[id].split("(")[0];
+    const styles = {};
+    styles["style"] = {};
+    styles["style"][id] = `${subProperty}(${prefix(
+      id,
+      e.target.value,
+      subProperty
+    )})`;
+    setstyle(bigImage, styles);
+  }
+}
+
+function createController(id, value, style) {
+  const inputContainer = document.createElement("div");
+  inputContainer.className = "input-container";
+
+  const inputContoroller = document.createElement("input");
+  inputContoroller.type = "range";
+  inputContoroller.min = "0";
+  inputContoroller.max = "100";
+  inputContoroller.step = "1";
+  inputContoroller.value = value;
+  inputContoroller.id = "inputFilterController" + captalize(id);
+  inputContoroller.className = "range-controller " + id;
+  inputContoroller.addEventListener("input", (e) =>
+    changeByRange(e, id, style)
+  );
+
+  const labelController = document.createElement("label");
+  labelController.for = "inputFilterContoroller" + captalize(id);
+  labelController.innerText = id;
+  labelController.className = "label-controller " + id;
+
+  inputContainer.append(labelController, inputContoroller);
+  return inputContainer;
 }
 
 ["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
@@ -88,3 +192,8 @@ function handleupload(e) {
 uploadForm.addEventListener("drop", dropHandler);
 
 document.getElementById("inputUpload").addEventListener("change", handleupload);
+
+controllerClose.addEventListener("click", () => {
+  controllerContainer.classList.remove("show");
+  controllerContainer.classList.add("hide");
+});
